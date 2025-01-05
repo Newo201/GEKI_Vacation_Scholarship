ea2a995c988b9c58b18d79214fb9b802953ded77
newo200 <newo200coding@gmail.com>
2025-01-06 10:57:38 +1100

    Minor code refactoring e.g. changing argument name from iterations to num_samples for generating data

--- src/samples_normal.R
+++ src/samples_normal.R
@@ -1,40 +1,45 @@
 ###################### Sampling From Priors #################################
 
 # Generate samples from the prior distributions
-alpha_prior_sample <- function(alpha.sd, iterations) {
-  return(rnorm(iterations, mean = 0, sd = alpha.sd))
+alpha_prior_sample <- function(alpha.sd, num_samples) {
+  return(rnorm(num_samples, mean = 0, sd = alpha.sd))
 }
 
 # We need to have a prior sample that is from (-infinity, infinity)
 # but we also need to restrict sigma2 to R+. Therefore we sample the log
 # prior from a normal distribution
-logsigma2_prior_sample <- function(sigma2.sd, iterations) {
+logsigma2_prior_sample <- function(sigma2.sd, num_samples) {
   
-  return(rnorm(iterations, mean = 0, sd = sigma2.sd))
+  return(rnorm(num_samples, mean = 0, sd = sigma2.sd))
 }
 
 ###################### Sampling From Likelihood ###############################
 
 # Generate samples from the normal distribution
-likelihood_sample <- function(alpha, x, sigma2) {
+likelihood_sample <- function(parameters, num_samples) {
+  alpha = parameters$alpha
+  x = parameters$x
+  sigma2 = parameters$sigma2
   dim = length(x)
   return(rmvnorm(1, mean = alpha*x, sigma = sigma2*diag(dim)))
 }
 
-# Generate simulated data using the true parameters
-generate_data <- function(iterations, parameters) {
-  alpha.true <- parameters$alpha
-  x.true <- parameters$x
-  sigma2.true <- parameters$sigma2
-  samples <- matrix(nrow = iterations, ncol = length(x.true))
-  print(alpha.true, x.true, sigma2.true)
-  for (i in 1:iterations) {
-    samples[i, ] <- likelihood_sample(alpha.true, x.true, sigma2.true)
-  }
-  return(samples)
-}
+# # Generate simulated data using the true parameters
+# generate_data <- function(iterations, parameters) {
+#   alpha.true <- parameters$alpha
+#   x.true <- parameters$x
+#   sigma2.true <- parameters$sigma2
+#   samples <- matrix(nrow = iterations, ncol = length(x.true))
+#   print(alpha.true, x.true, sigma2.true)
+#   for (i in 1:iterations) {
+#     samples[i, ] <- likelihood_sample(alpha.true, x.true, sigma2.true)
+#   }
+#   return(samples)
+# }
 
 
-# true_params = list(alpha = 2, sigma2 = 2, x = c(1, 1))
-# test <- generate_data(100, true_params)
+# parameters = list(alpha = 2, sigma2 = 2, x = c(1, 1))
+# test <- generate_data(100, parameters)
 # likelihood_sample()
+
+test <- rmvnorm(10, mean = c(1, 0, 1), sigma = diag(3))
