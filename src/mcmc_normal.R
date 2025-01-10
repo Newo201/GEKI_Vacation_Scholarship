f11660f5fd03107d7134bab8340ccbab348de165
newo200 <newo200coding@gmail.com>
2025-01-10 17:11:43 +1100

    Investigate large sample limits of covariance matrices in known variance case

--- src/mcmc_normal.R
+++ src/mcmc_normal.R
@@ -17,15 +17,15 @@ lupost_normal <- function(y, theta.init, x, prior_params) {
 normal_mcmc <- function(true_params, prior_params, iterations = 1e4) {
   
   # 1. Generate data given true parameters
-  simulated_data <- likelihood_sample(true_params, 1)
+  simulated_data <- likelihood_normal(true_params)
   
   # 2. Create a partial function fixing data and parameters
   lupost_normal_mcmc <- partial(lupost_normal, y = simulated_data, 
-                                x = true_params$x, params = prior_params)
+                                x = true_params$x, prior_params = prior_params)
   
   # 3. Draw parameters from prior distribution
-  alpha.init <- alpha_prior_sample(prior_params$alpha.sd, 1)
-  logsigma2.init <- logsigma2_prior_sample(prior_params$sigma2.sd, 1)
+  alpha.init <- alpha_prior_sample(prior_params, 1)
+  logsigma2.init <- logsigma2_prior_sample(prior_params, 1)
   theta.init <- c(alpha.init, logsigma2.init)
   
   # 4. Call on MCMC function
