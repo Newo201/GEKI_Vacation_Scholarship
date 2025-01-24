fc5f9215fded823fae12791166dcfbf2703e1b4c
newo200 <newo200coding@gmail.com>
2025-01-24 14:56:28 +1100

    Add models for lognormal distribution

---
+++ src/samples/samples_lognormal.R
@@ -0,0 +1,33 @@
+###################### Sampling From Priors #################################
+
+# Generate samples from the prior distributions
+alpha_prior_sample <- function(parameters, num_samples) {
+  mean = parameters$alpha.mean
+  sd = parameters$alpha.sd
+  return(rnorm(num_samples, mean = mean, sd = sd))
+}
+
+# We need to have a prior sample that is from (-infinity, infinity)
+# but we also need to restrict sigma2 to R+. Therefore we sample the log
+# prior from a normal distribution
+logsigma2_prior_sample <- function(parameters, num_samples) {
+  
+  mean = parameters$sigma2.mean
+  sd = parameters$sigma2.sd
+  
+  return(rnorm(num_samples, mean = mean, sd = sd))
+}
+
+###################### Sampling From Likelihood ###############################
+
+# Generate samples from the normal distribution
+likelihood_lognormal <- function(parameters) {
+  alpha = parameters$alpha
+  x = parameters$x
+  sigma = parameters$sigma
+  dim = length(x)
+  # print(dim)
+  # print(sigma2)
+  # print(sigma2*diag(dim))
+  return(rlnorm(length(x), mean = alpha*x, sd = sigma))
+}
